be881630c36e8c3a419564b4c537ad956719599d
ravivarmalearner <pbmravivarma@gmail.com>
2019-11-18 10:27:30 +0000

    added user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191113044924) do
+ActiveRecord::Schema.define(version: 20191118064151) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,4 +19,20 @@ ActiveRecord::Schema.define(version: 20191113044924) do
     t.datetime "updated_at"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  
 end
