ef88534e5312d192f1edc622505611cd4c740594
ravivarmalearner <pbmravivarma@gmail.com>
2019-11-19 09:10:46 +0000

    created userarticle-association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-    
+    has_many :articles
+    validates :username,
+        presence: true, 
+        uniqueness: true, 
+        length: {minimum: 3, maximum: 20}
+    validates :email,
+        presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191118064151) do
+ActiveRecord::Schema.define(version: 20191119085135) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,5 +26,5 @@ ActiveRecord::Schema.define(version: 20191118064151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
 end
